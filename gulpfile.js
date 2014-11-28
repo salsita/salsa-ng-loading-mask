b37f4b5b3e13bb6078a3d793f337af790c1943fd
Tomas Weiss <tomasw@salsitasoft.com>
2014-11-28 17:31:38 +0100

    dist build & travis support

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
     karma = require('karma').server;
 
 gulp.task('test', function(done) {
-    karma.start({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: false
-    }, done);
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: false
+  }, done);
 });
 
 gulp.task('build', function() {
+  gulp.src('src/loadingMask.js')
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 
+  gulp.src('src/loadingMask.css')
+    .pipe(minifyCss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['build']);
